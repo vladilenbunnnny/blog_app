c3abd11565e6163ceee3b41d25d4c882b9ddb35a
thebunnnny <thebunnnny@gmail.com>
2021-11-10 12:24:34 +1000

    routing

---
+++ app/Main.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//My Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"));
+
+if (module.hot) {
+  module.hot.accept();
+}
