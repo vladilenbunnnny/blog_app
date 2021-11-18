c55d6366ed18e96f2b94e2c6a1f45f7ede7f56de
thebunnnny <thebunnnny@gmail.com>
2021-11-18 12:15:18 +1000

    route for profile picture that takes to profile component

---
+++ app/components/Profile.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Page from "./Page";
+
+function Profile() {
+  return (
+    <Page title="Profile">
+      <h1>Profile</h1>
+    </Page>
+  );
+}
+
+export default Profile;
