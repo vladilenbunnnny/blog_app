4e7f2811db5e1e6ad74a944c76e55d5ff7522285
thebunnnny <thebunnnny@gmail.com>
2021-11-19 16:08:10 +1000

    Profile component populated, axios request done

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,10 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import StateContext from "../StateContext";
 import Page from "./Page";
+import { useParams } from "react-router";
+import Axios from "axios";
 
 function Profile() {
+  const [profileData, setProfileData] = useState({
+    profileUsername: "...",
+    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+    isFollowing: false,
+    counts: {
+      postCount: "",
+      followerCount: "",
+      followingCount: "",
+    },
+  });
+  const { username } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token });
+        console.log(response);
+        setProfileData(response.data);
+      } catch (error) {
+        console.log("There was an error");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const appState = useContext(StateContext);
   return (
     <Page title="Profile">
-      <h1>Profile</h1>
+      <h2>
+        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
+        <button className="btn btn-primary btn-sm ml-2">
+          Follow <i className="fas fa-user-plus"></i>
+        </button>
+      </h2>
+
+      <div className="profile-nav nav nav-tabs pt-2 mb-4">
+        <a href="#" className="active nav-item nav-link">
+          Posts: {profileData.counts.postCount}
+        </a>
+        <a href="#" className="nav-item nav-link">
+          Followers: {profileData.counts.followerCount}
+        </a>
+        <a href="#" className="nav-item nav-link">
+          Following: {profileData.counts.followingCount}
+        </a>
+      </div>
+
+      <div className="list-group">
+        <a href="#" className="list-group-item list-group-item-action">
+          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
+          <span className="text-muted small">on 2/10/2020 </span>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #2</strong>
+          <span className="text-muted small">on 2/10/2020 </span>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
+          <span className="text-muted small">on 2/10/2020 </span>
+        </a>
+      </div>
     </Page>
   );
 }
