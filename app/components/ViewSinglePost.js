80b46b98bceb3df20cc1524128b633424f59b292
thebunnnny <thebunnnny@gmail.com>
2021-11-20 17:54:53 +1000

    date added to post

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,15 +1,14 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import StateContext from "../StateContext";
 import Page from "./Page";
 import Axios from "axios";
 
 function ViewSinglePost() {
-  const appState = useContext(StateContext);
-  const name = appState.user.username;
   const { id } = useParams();
   const [post, setPost] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [date, setDate] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,6 +16,7 @@ function ViewSinglePost() {
         const response = await Axios.get(`/post/${id}`);
         console.log("Success loading single post");
         setPost(response.data);
+        setDate(response.data.createdDate);
         console.log(response);
         setIsLoading(false);
       } catch (error) {
@@ -25,32 +25,39 @@ function ViewSinglePost() {
     };
     fetchData();
   }, []);
-  return isLoading ? (
-    <h4>Loading...</h4>
-  ) : (
+
+  const extractedDate = new Date(date);
+  const dateFormatted = `${extractedDate.getDate()}/${extractedDate.getMonth() + 1}/${extractedDate.getFullYear()}`;
+  return (
     <Page title="Post">
-      <div className="d-flex justify-content-between">
-        <h2>{post.title}</h2>
-        <span className="pt-2">
-          <a href="#" className="text-primary mr-2" title="Edit">
-            <i className="fas fa-edit"></i>
-          </a>
-          <a className="delete-post-button text-danger" title="Delete">
-            <i className="fas fa-trash"></i>
-          </a>
-        </span>
-      </div>
+      {isLoading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <>
+          <div className="d-flex justify-content-between">
+            <h2>{post.title}</h2>
+            <span className="pt-2">
+              <a href="#" className="text-primary mr-2" title="Edit">
+                <i className="fas fa-edit"></i>
+              </a>
+              <a className="delete-post-button text-danger" title="Delete">
+                <i className="fas fa-trash"></i>
+              </a>
+            </span>
+          </div>
 
-      <p className="text-muted small mb-4">
-        <a href="#">
-          <img className="avatar-tiny" src={post.author.avatar} />
-        </a>
-        Posted by <a href="#">{post.author.username}</a> on 2/10/2020
-      </p>
+          <p className="text-muted small mb-4">
+            <a href="#">
+              <img className="avatar-tiny" src={post.author.avatar} />
+            </a>
+            Posted by <a href="#">{post.author.username}</a> on {dateFormatted}
+          </p>
 
-      <div className="body-content">
-        <p>{post.body}</p>
-      </div>
+          <div className="body-content">
+            <p>{post.body}</p>
+          </div>
+        </>
+      )}
     </Page>
   );
 }
